c4aa07c40d3eaa46d675f03dc7b036e55e0aeb52
Sean Wang <x.wang1029@gmail.com>
2025-03-26 19:45:04 -0700

    fix deploy

--- site.config.ts
+++ site.config.ts
@@ -1,4 +1,4 @@
-export const siteUrl = "";
+export const siteUrl = "https://ailabs-capital.netlify.app/";
 export const siteTitle = "AiLabs Capital";
 export const defaultLocale = "en";
 export const locales = ["en"];
